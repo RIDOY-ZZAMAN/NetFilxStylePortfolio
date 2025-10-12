5a83a6360c7bb877efbb1734a25ae39f381a6aed
RIDOY-ZZAMAN <ridoyzzaman957@outlook.com>
2025-10-13 01:56:42 +0800

    aktaruzzamanridoy.netlify.app was added in case

--- src/queries/getDatoCmsToken.ts
+++ src/queries/getDatoCmsToken.ts
@@ -6,6 +6,7 @@ export const getDatoCmsToken = () => {
     case "my-portfolio-85.admin.datocms.com":
     case "ror.localhost":
     case "localhost":
+    case "aktaruzzamanridoy.netlify.app":
       return "7770e981b18c02ccf39b9f440cd548";
     default:
       throw new Error(`No DatoCMS token configured for hostname: ${hostname}`);
